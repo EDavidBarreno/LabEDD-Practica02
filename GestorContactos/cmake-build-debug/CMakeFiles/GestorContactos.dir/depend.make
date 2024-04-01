9d8b4fcf0a40989a18be0c908bbfc44de4178268
Pinto324 <branps18@gmail.com>
2024-03-31 18:39:02 -0600

    primer cambio

---
+++ GestorContactos/cmake-build-debug/CMakeFiles/GestorContactos.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GestorContactos.
+# This may be replaced when dependencies are built.
--- GestorContactos/main.cpp
+++ GestorContactos/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "XDXD" << std::endl;
     return 0;
 }
