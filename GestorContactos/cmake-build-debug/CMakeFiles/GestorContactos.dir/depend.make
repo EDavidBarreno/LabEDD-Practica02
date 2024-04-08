d419724105118662df061f872c6b101508d6426f
EDavidBarreno <davidbarreno13@gmail.com>
2024-04-07 21:35:24 -0600

    1

--- GestorContactos/cmake-build-debug/CMakeFiles/GestorContactos.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GestorContactos.
-# This may be replaced when dependencies are built.
--- GestorContactos/cmake-build-debug/cmake_install.cmake
+++ GestorContactos/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/david/Documents/CUNOC/Ingenieria en Ciencias y Sistemas/2024/01-Senestre/Estructura de Datos/Lab - EDD/Practica 02/Practica02/GestorContactos
+# Install script for directory: C:/Users/david/Desktop/LabEDD-Practica02/GestorContactos
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/david/Documents/CUNOC/Ingenieria en Ciencias y Sistemas/2024/01-Senestre/Estructura de Datos/Lab - EDD/Practica 02/Practica02/GestorContactos/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/david/Desktop/LabEDD-Practica02/GestorContactos/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
